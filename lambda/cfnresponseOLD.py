790b75651d62290fa26be8cffcdf48a67adc6e1a
Christopher Reynolds <creynolds@ot-creynolds-mbp.local>
2023-02-17 11:42:39 -0600

    added updated files

--- lambda/cfnresponseOLD.py
+++ lambda/cfnresponseOLD.py
@@ -1,13 +1,17 @@
 #  Copyright 2016 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
-#  SPDX-License-Identifier: Apache-2.0
+#  This file is licensed to you under the AWS Customer Agreement (the "License").
+#  You may not use this file except in compliance with the License.
+#  A copy of the License is located at http://aws.amazon.com/agreement/ .
+#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+#  See the License for the specific language governing permissions and limitations under the License.
 
-import urllib3
+from botocore.vendored import requests
 import json
-http = urllib3.PoolManager()
+
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
-def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
+def send(event, context, responseStatus, responseData, physicalResourceId=None):
     responseUrl = event['ResponseURL']
 
     print(responseUrl)
@@ -19,7 +23,6 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     responseBody['StackId'] = event['StackId']
     responseBody['RequestId'] = event['RequestId']
     responseBody['LogicalResourceId'] = event['LogicalResourceId']
-    responseBody['NoEcho'] = noEcho
     responseBody['Data'] = responseData
 
     json_responseBody = json.dumps(responseBody)
@@ -32,8 +35,9 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     }
 
     try:
-        
-        response = http.request('PUT',responseUrl,body=json_responseBody.encode('utf-8'),headers=headers)
+        response = requests.put(responseUrl,
+                                data=json_responseBody,
+                                headers=headers)
         print("Status code: " + response.reason)
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
